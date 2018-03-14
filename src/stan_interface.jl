fca48a5bdc2616385c8bba88cc01d024fe14886e
BriceonWiley <briceon_wiley@baylor.edu>
2018-03-14 11:31:20 -0500

    You're still doing a great job. Just another suggestion.

--- src/stan_interface.jl
+++ src/stan_interface.jl
@@ -17,11 +17,13 @@ This function calls rstan. Arguments:
     You may want to set your editor first, eg:
     ENV["EDITOR"] = "code"
 """
-function rstan(model_code, data; nchains = 4, adapt_delta = 0.8, max_treedepth = 10)
+function rstan(model_code, data, iter = 1000; pars = missing, qnchains = 4, adapt_delta = 0.8, max_treedepth = 10)
     @rput model_code
     @rput data
     @rput nchains
-    R"res <- stan(model_code = model_code, data = data, chains = nchains, control = list(adapt_delta = $adapt_delta, max_treedepth = $max_treedepth))"
+    @rput iter
+    @rput pars
+    R"res <- stan(model_code = model_code, data = data, iter = iter, pars = pars, chains = nchains, control = list(adapt_delta = $adapt_delta, max_treedepth = $max_treedepth))"
     R"samples <- rstan::extract(res)"
     println(R"res")
     @rget samples
